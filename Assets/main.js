6c54706a1f092bf1a56dc88ba09f39538ea98f82
Rhea Patel <rheapatel@Rheas-MacBook-Air.local>
2024-08-31 19:26:24 +0530

    3rd commit

---
+++ Assets/main.js
@@ -0,0 +1,7 @@
+
+        document.addEventListener('DOMContentLoaded', (event) => {
+            const video = document.getElementById('bg-vid');
+
+            // Set the playback speed (0.5 is half the normal speed, 2 is twice the normal speed)
+            video.playbackRate = 0.35; 
+        });
